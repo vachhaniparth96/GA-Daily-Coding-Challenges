c6957d846607686b3e961adf70e133d1019b77c9
Parth Vachhani <vachhani.parth.96@gmail.com>
2023-10-18 16:51:47 -0400

    Completed second coding challenge: Add One

--- codeChallenge.js
+++ codeChallenge.js
@@ -12,3 +12,19 @@ function sayHello() {
     return 'Hello!'
 } 
 console.log(sayHello());
+
+/*-----------------------------------------------------------------
+Challenge: 01-addOne
+Difficulty: Basic
+Prompt:
+Write a function called addOne that takes a single number as an argument and returns that number plus 1.
+Examples:
+addOne(1) //=> 2
+addOne(-5) //=> -4
+-----------------------------------------------------------------*/
+// Your solution for 01-addOne here:
+
+function addOne(num) {
+ return  num + 1;
+} 
+console.log(addOne(-5));
