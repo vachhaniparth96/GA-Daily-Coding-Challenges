819b5c474db4f17802a720c2093fe79ec1e82b19
Parth Vachhani <vachhani.parth.96@gmail.com>
2023-10-18 17:00:26 -0400

    Completed Coding Challenge 3: Add Two Numbers

--- codeChallenge.js
+++ codeChallenge.js
@@ -28,3 +28,27 @@ function addOne(num) {
  return  num + 1;
 } 
 console.log(addOne(-5));
+
+/*-----------------------------------------------------------------
+Challenge: 02-addTwoNumbers
+Difficulty: Basic  
+Prompt:
+Write a function called addTwoNumbers that accepts two numeric arguments and returns the sum of those two numbers.
+If either argument is not a Number, return the value of NaN.
+Examples:
+addTwoNumbers(5, 10) //=> 15
+addTwoNumbers(10, -2) //=> 8
+addTwoNumbers(0, 0) //=> 0
+addTwoNumbers('Hello', 5) //=> NaN
+-----------------------------------------------------------------*/
+// Your solution for 02-addTwoNumbers here:
+
+function addTwoNumbers(num1, num2) {
+    if(typeof(num1 + num2) === 'number'){
+    return num1 + num2;
+    }
+    else {
+        return NaN;
+    }
+}
+console.log(addTwoNumbers(6,10));
