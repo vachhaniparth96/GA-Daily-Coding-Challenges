8cd2cbfb43f88f5d22b5cdd4628ba2769f8f1c13
Parth Vachhani <vachhani.parth.96@gmail.com>
2023-10-18 16:43:15 -0400

    Completed first challenge: Say Hello

---
+++ codeChallenge.js
@@ -0,0 +1,14 @@
+/*-----------------------------------------------------------------
+Challenge: 00-sayHello (example)
+Difficulty: Basic
+Prompt:
+Write a function called sayHello that returns the string "Hello!".
+Examples:
+sayHello() //=> Hello!
+-----------------------------------------------------------------*/
+// Your solution for 00-sayHello (example) here:
+
+function sayHello() {
+    return 'Hello!'
+} 
+console.log(sayHello());
