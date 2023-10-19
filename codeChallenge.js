71df7d697012bf6c411a30ad82792e958667820d
Parth Vachhani <vachhani.parth.96@gmail.com>
2023-10-19 16:58:35 -0400

    completed DCC #3 and #4

--- codeChallenge.js
+++ codeChallenge.js
@@ -52,3 +52,51 @@ function addTwoNumbers(num1, num2) {
     }
 }
 console.log(addTwoNumbers(6,10));
+
+/*-----------------------------------------------------------------
+Challenge: 03-sumNumbers
+Difficulty: Basic
+Prompt:
+- Write a function called sumNumbers that accepts a single array of numbers and returns the sum of the numbers in the array.
+- If the array is empty, return 0 (zero).
+Examples:
+sumNumbers([10]) //=> 10
+sumNumbers([5, 10]) //=> 15
+sumNumbers([2, 10, -5]) //=> 7
+sumNumbers([]) //=> 0
+-----------------------------------------------------------------*/
+// Your solution for 03-sumNumbers here:
+
+function sumNumbers(array) {
+    let sum = 0;
+    array.forEach(function(a) {
+        sum+= a;
+    })
+    return sum;
+}
+console.log(sumNumbers([]));
+/*-----------------------------------------------------------------
+Challenge: 04-addList
+Difficulty: Basic
+Prompt:
+- Write a function called addList that accepts any quantity of numbers as arguments, adds them together and returns the resulting sum.
+- Assume all parameters will be numbers.
+- If called with no arguments, return 0 (zero).
+Examples:
+add(1) //=> 1
+add(1, 50, 1.23) //=> 52.23
+add(7, -12) //=> -5
+
+Hint:  Check out the Further Study section of the JS Functions lesson regarding "rest parameters"
+-----------------------------------------------------------------*/
+// Your solution for 04-addList here:
+
+function addList() {
+    let sum = 0
+    for(let i=0;i<arguments.length;i++) {
+        sum+=arguments[i]
+    }
+    return sum;
+}
+
+console.log(addList(1,50,1.23));
